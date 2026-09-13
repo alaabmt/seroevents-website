cb43d343d2c69e9122ce8d3f3c75eb740cd115dc
alaabmt <160816140+alaabmt@users.noreply.github.com>
2026-09-13 18:04:32 +0400

    Add Partners to shared navigation

--- app/components/SiteNav.tsx
+++ app/components/SiteNav.tsx
@@ -7,6 +7,7 @@ export function SiteNav() {
         <a href="/seroevents-website/services/">Services</a>
         <a href="/seroevents-website/events/">Events</a>
         <a href="/seroevents-website/gallery/">Gallery</a>
+        <a href="/seroevents-website/partners/">Partners</a>
         <a href="/seroevents-website/contact/">Contact</a>
       </nav>
       <a className="subCta" href="/seroevents-website/events/upcoming/">Upcoming Events</a>
@@ -21,6 +22,7 @@ export function SiteFooter() {
       <div className="subFooterLinks">
         <a href="/seroevents-website/events/upcoming/">Upcoming</a>
         <a href="/seroevents-website/events/past/">Past Events</a>
+        <a href="/seroevents-website/partners/">Partners</a>
         <a href="mailto:info@seroevents.com">info@seroevents.com</a>
       </div>
     </footer>
