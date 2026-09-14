e8cde50364cff1c289c31a171ade3574e78597db
alaabmt <160816140+alaabmt@users.noreply.github.com>
2026-09-14 16:54:40 +0400

    Add robots directives and sitemap reference

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://alaabmt.github.io/seroevents-website/sitemap.xml',
+    host: 'https://alaabmt.github.io/seroevents-website',
+  };
+}
